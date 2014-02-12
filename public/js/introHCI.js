02bc5153383c057443b8c15e29a4caa54a674531
Cobra Kao <ru6xul6@gmail.com>
2014-02-12 15:07:39 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,6 +26,17 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+  $.get('/project/' + idNumber, function(result) {
+    var projectHTML = '<a href="#" class="thumbnail">' +
+        '<img src="' + result['image'] + '" class="detailsImage">' +
+        '<p>' + result['title'] + '</p>' +
+        '<p><small>' + result['date'] + '</small></p></a>' +
+        '<p>' + result['summary'] + '</p>';
+    $("#project" + idNumber + " .details").html(projectHTML);
+    console.log($("#project" + idNumber + " .details"));
+    console.log(projectHTML);
+  });
+
 	console.log("User clicked on project " + idNumber);
 }
 
@@ -34,5 +45,13 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+  $.get('/palette', function(result) {
+    var colors = result['colors']['hex']
+    $('body').css('background-color', colors[0]);
+    $('.thumbnail').css('background-color', colors[1]);
+    $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+    $('p').css('color', colors[3]);
+    $('.project img').css('opacity', .75);
+  });
 	console.log("User clicked on color button");
 }
